ee92b2d1c55bc9b469f25935d19a030037c273c1
aiwebdevs90 <aiwebdevs90@gmail.com>
2021-11-21 01:23:00 +0000

    New - Extension files added

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('prox5/', views.prox5, name='prox5'),
+    path('photogrammetry/', views.photogrammetry, name='photogrammetry'),
+    path('telecomsdatabase/', views.telecomsdatabase, name='telecomsdatabase'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,28 @@ from django.shortcuts import render
 def index(request):
     """ A View to return the index page """
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ A View to return the index page """
+    return render(request, 'home/about.html')
+
+
+def services(request):
+    """ A View to return the index page """
+    return render(request, 'home/services.html')
+
+
+def prox5(request):
+    """ A View to return the index page """
+    return render(request, 'home/prox5.html')
+
+
+def photogrammetry(request):
+    """ A View to return the index page """
+    return render(request, 'home/photogrammetry.html')
+
+
+def telecomsdatabase(request):
+    """ A View to return the index page """
+    return render(request, 'home/telecomsdatabase.html')
